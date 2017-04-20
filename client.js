a373ac2790cfb5fa0a2952a7b83f42293f69d1da
Lance Elliott <delliott002@satx.rr.com>
2017-04-19 23:48:30 -0500

    wolfram and youtube. auth json.
    added wolfram alpha and youtube. set google key. have not downloaded npm
    google yet.

--- client.js
+++ client.js
@@ -6,10 +6,18 @@
 
 var RtmClient = require('@slack/client').RtmClient;
 var RTM_EVENTS = require('@slack/client').RTM_EVENTS;
+var Wolfram = require('node-wolfram');
+var AuthDetails = require("./auth.json");
+var YouTube = require('youtube-node');
 
-var token = process.env.SLACK_API_TOKEN || 'xoxb-171447572308-vBpKWvbhghI5NTQq9kSWIQSz';
+var token = process.env.SLACK_API_TOKEN || '';
 
 var rtm = new RtmClient(token, { logLevel: 'debug' });
+var youTube = new YouTube();
+var wolfram = new Wolfram(AuthDetails.wolfram_api_key)
+
+
+youTube.setKey(AuthDetails.youtube_api_key);
 rtm.start();
 
 
@@ -23,55 +31,157 @@ function DisplayHelp(message)
 {
 	var Output = "Help List: \n";
 	
-	Output += "/help - Help \n";
+	Output += "!help - Help \n";
 	Output += "hello - Greetings \n";
-	Output += "/calc - calculate rest of text \n";
-	Output += "/name - Get my name \n";
-	Output += "/about - About me \n";
-	Output += "/cat - Yes. \n";
+	Output += "!calc - calculate rest of text \n";
+	Output += "!name - Get my name \n";
+	Output += "!about - About me \n";
+	Output += "!cat - Yes. \n";
+	Output += "!video - Youtube Search";
 	
 	PostMessage(Output, message.channel);
 }
+
+function YoutubeSearch(message) 
+{
+	youTube.search(message.text, 1, function(error, result) {
+		if (error) {
+			PostMessage(error, message.channel);
+		}
+		else if (!result || !result.items || result.items.length < 1) {
+			PostMessage("¯\\_(ツ)_/¯", message.channel);
+		} 
+		else {
+			var YTlink = "http://www.youtube.com/watch?v=" + result.items[0].id.videoId;
+			PostMessage(YTlink, message.channel);
+		}
+	});
+}
+
+function WolframQuery(message) {
+	wolfram.query(message.text, function(error, result) {
+			if (error) {
+				console.log(error);
+				PostMessage("Couldn't talk to Wolfram Alpha :(", message.channel);
+			} else {
+				console.log(JSON.stringify(result));
+				var response = "";
+				if(result.queryresult.$.success == "true"){
+					if(result.queryresult.hasOwnProperty("warnings")){
+						for(var i in result.queryresult.warnings){
+							for(var j in result.queryresult.warnings[i]){
+								if(j != "$"){
+									try {
+										PostMessage(result.queryresult.warnings[i][j][0].$.text, message.channel);
+									} catch(e){
+										console.log("WolframAlpha: failed displaying warning:\n"+e.stack());
+									}
+								}
+							}
+						}
+					}
+					if(result.queryresult.hasOwnProperty("assumptions")){
+						for(var i in result.queryresult.assumptions){
+							for(var j in result.queryresult.assumptions[i]){
+								if(j == "assumption"){
+									try {
+										PostMessage(`Assuming ${result.queryresult.assumptions[i][j][0].$.word} is ${result.queryresult.assumptions[i][j][0].value[0].$.desc}`, message.channel);
+									} catch(e) {
+										console.log("WolframAlpha: failed displaying assumption:\n"+e.stack());
+									}
+								}
+							}
+						}
+					}
+					if(result && result.queryresult && result.queryresult.pod)
+					{
+						for(var a=0; a<result.queryresult.pod.length; a++)
+						{
+							var pod = result.queryresult.pod[a];
+							response += "**"+pod.$.title+"**:\n";
+							for(var b=0; b<pod.subpod.length; b++)
+							{
+								var subpod = pod.subpod[b];
+												//can also display the plain text, but the images are prettier
+								/*for(var c=0; c<subpod.plaintext.length; c++)
+								{
+									response += '\t'+subpod.plaintext[c];
+								}*/
+												for(var d=0; d<subpod.img.length;d++)
+												{
+													response += "\n" + subpod.img[d].$.src;
+													PostMessage(response, message.channel);
+													response = "";
+												}
+							}
+										response += "\n";
+						}
+					}
+					else{
+						PostMessage("¯\\_(ツ)_/¯", message.channel);
+					}
+				}	else {
+					if(result.queryresult.hasOwnProperty("didyoumeans")){
+						var msg = [];
+						for(var i in result.queryresult.didyoumeans){
+							for(var j in result.queryresult.didyoumeans[i].didyoumean) {
+								msg.push(result.queryresult.didyoumeans[i].didyoumean[j]._);
+							}
+						}
+						PostMessage("Did you mean: " + msg.join(" "), message.channel);
+					} else {
+						PostMessage("No results from Wolfram Alpha :(", message.channel);
+					}
+				}
+			}
+		});
+};
 
 
 function ProcessMessage(message) 
 {
 	if(message.text.includes("!h"))
 	{
-		message.text.replace("!h", "");
+		message.text = message.text.replace("!h", "");
 		DisplayHelp(message);
 	}
-	else if(message.text.includes("!hello"))
+	else if(message.text.includes("!video"))
 	{
-		message.text.replace("!hello", "");
-		PostMessage("Sup Homie", message.channel);
+		message.text = message.text.replace("!video", "");
+		YoutubeSearch(message);
 	}
 	else if(message.text.includes("!calc"))
 	{
-		message.text.replace("!calc", "");
-		PostMessage("WIP", message.channel);
+		message.text = message.text.replace("!calc", "");
+		
+		WolframQuery(message);
 	}
 	else if(message.text.includes("!name"))
 	{
-		message.text.replace("!name", "");
+		message.text = message.text.replace("!name", "");
 		PostMessage("My name es JEFF", message.channel);
 	}
 	else if(message.text.includes("!about"))
 	{
-		message.text.replace("!about", "");
+		message.text = message.text.replace("!about", "");
 		PostMessage("This is the about me section", message.channel);
 	}
 	else if(message.text.includes("!cat"))
 	{
-		message.text.replace("!cat", "");
+		message.text = message.text.replace("!cat", "");
 		PostMessage("https://static.pexels.com/photos/104827/cat-pet-animal-domestic-104827.jpeg", message.channel);
+	}
+	else if(message.text.includes("hello"))
+	{
+		message.text = message.text.replace("hello", "");
+		PostMessage("Sup Homies", message.channel);
 	}
 }
 
 
 rtm.on(RTM_EVENTS.MESSAGE, function handleRtmMessage(message) {
   console.log('Message:', message);
-  if(message.user != rtm.activeUserId )
+  if(message.user != rtm.activeUserId && message.text && message.text.length > 0)
   {
 	ProcessMessage( message );
   }
